38da6507ee4040ccbcccc594d15ab10049b465c5
pol <pol.monso@somenergia.coop>
2022-11-22 12:29:22 +0100

    alert_meter_zero_energy only checks daylight readings

--- dbt_jardiner/models/alerts/alert_meter_zero_energy.sql
+++ dbt_jardiner/models/alerts/alert_meter_zero_energy.sql
@@ -1,5 +1,28 @@
 {{ config(materialized='view') }}
 
+{#
+Explanation of this query
+
+meterregistry_last_readings selects the last 30 days to speedup
+
+meterregistry_daylight joins with the sunrise and sunset for each reading
+
+meter_registry_hourly_raw selects _only_ the last `num_hours_threshold` readings
+for each meter (which is typically 1 for big plants and 4 for small plants)
+
+meter_registry_group groups each group of readings by meter to find out
+if all readings were at 0 which will then trigger the alarm
+
+NULL values are not possible unless we have no readings at all in the last 30 days,
+otherwise it will always select the last N readings in daylight
+regardless of which day they come from. It will even combine different days.
+
+The merging script which notifies and feeds alert_X_historic alert_X_status
+sets to false alarms with NULL
+
+#}
+
+
 with meterregistry_last_readings as (
     select * from {{ ref('meter_registry_raw') }}
     where current_date - interval '30 days' < time
@@ -41,6 +64,7 @@ meter_registry_hourly_raw as (
     from meterregistry_daylight as mr
     where
         mr.meter_id = meters.meter_id
+        and mr.time between mr.daylight_start and mr.daylight_end
     order by time desc
     limit meters.num_hours_threshold
   ) as mr on true
@@ -60,11 +84,7 @@ meter_registry_group as (
     meter_connection_protocol,
     count(export_energy_wh) as reading_count,
     max(export_energy_wh) as export_energy_wh,
-    case
-      when every(is_daylight) then max(export_energy_wh) = 0
-      when not every(is_daylight) then NULL -- if not daylight we don't know 
-      else NULL
-    end as alarm_zero_energy
+    max(export_energy_wh) = 0 as alarm_zero_energy
   from meter_registry_hourly_raw
   group by plant_id, meter_id, meter_name, meter_connection_protocol, num_hours_threshold
 )
