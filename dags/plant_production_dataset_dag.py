8ad7a7bd98a5c5161642c3d90a43775c47be5434
Roger <roger.sanjaume@somenergia.coop>
2023-01-10 15:41:58 +0100

    DAG plant production daily and monthly

--- dags/plant_production_dataset_dag.py
+++ dags/plant_production_dataset_dag.py
@@ -29,19 +29,14 @@ nfs_config = {
 driver_config = DriverConfig(name='local', options=nfs_config)
 mount_nfs = Mount(source="local", target="/repos", type="volume", driver_config=driver_config)
 
-with DAG(dag_id='plantmonitor_monthly_production_dataset_dag', start_date=datetime(2023,1,10), schedule_interval='@dayly', catchup=False, tags=["Plantmonitor","Jardiner", "Transform"], default_args=args) as dag:
+with DAG(dag_id='plantmonitor_production_dataset_dag', start_date=datetime(2023,1,10), schedule_interval='@daily', catchup=False, tags=["Plantmonitor","Jardiner", "Transform", "DBT"], default_args=args) as dag:
 
     repo_name = 'somenergia-jardiner'
 
     task_check_repo = build_check_repo_task(dag=dag, repo_name=repo_name)
     task_git_clone = build_git_clone_ssh_task(dag=dag, repo_name=repo_name)
-    task_branch_pull_ssh = build_branch_pull_ssh_task(dag=dag, task_name='dbt_dayly_to_monthly_transformation_task', repo_name=repo_name)
+    task_branch_pull_ssh = build_branch_pull_ssh_task(dag=dag, task_name='dbt_daily_to_daily_transformation_task', repo_name=repo_name)
     task_update_image = build_update_image_task(dag=dag, repo_name=repo_name)
-
-    # fragile dbapi to user-password combination
-
-    # dbapi = '{{ var.value.plantmonitor_db }}'
-    # 	
 
     environment = {
         'DBUSER': '{{ var.value.plantmonitor_db_user }}',
@@ -51,9 +46,26 @@ with DAG(dag_id='plantmonitor_monthly_production_dataset_dag', start_date=dateti
         'DBNAME': 'plants'
     }
 
-    dbt_dayly_to_monthly_transformation_task = DockerOperator(
+
+    dbt_daily_to_daily_transformation_task = DockerOperator(
         api_version='auto',
-        task_id='dbt_dayly_to_monthly_transformation_task',
+        task_id='dbt_daily_to_daily_transformation_task',
+        docker_conn_id='somenergia_registry',
+        environment=environment,
+        image='{}/{}-requirements:latest'.format('{{ conn.somenergia_registry.host }}', repo_name),
+        working_dir=f'/repos/{repo_name}/dbt_jardiner',
+        command='dbt run --profiles-dir config --select +plant_production_daily',
+        docker_url=Variable.get("generic_moll_url"),
+        mounts=[mount_nfs],
+        mount_tmp_dir=False,
+        auto_remove=True,
+        retrieve_output=True,
+        trigger_rule='none_failed',
+    )
+
+    dbt_daily_to_monthly_transformation_task = DockerOperator(
+        api_version='auto',
+        task_id='dbt_daily_to_monthly_transformation_task',
         docker_conn_id='somenergia_registry',
         environment=environment,
         image='{}/{}-requirements:latest'.format('{{ conn.somenergia_registry.host }}', repo_name),
@@ -70,6 +82,7 @@ with DAG(dag_id='plantmonitor_monthly_production_dataset_dag', start_date=dateti
     task_check_repo >> task_git_clone
     task_check_repo >> task_branch_pull_ssh
     task_git_clone >> task_update_image
-    task_branch_pull_ssh >> dbt_dayly_to_monthly_transformation_task
+    task_branch_pull_ssh >> dbt_daily_to_daily_transformation_task
     task_branch_pull_ssh >> task_update_image
-    task_update_image >> dbt_dayly_to_monthly_transformation_task
+    task_update_image >> dbt_daily_to_daily_transformation_task
+    dbt_daily_to_daily_transformation_task >> dbt_daily_to_monthly_transformation_task
