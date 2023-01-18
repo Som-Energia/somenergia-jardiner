dfa03bdfcc087f01256311916554d216f0e5fcc2
Lugadur <lucia.garcia@somenergia.coop>
2023-01-18 10:18:18 +0100

    remove old comments

--- dbt_jardiner/models/operational/inverter/stg/lake_modbusreadings_inverter_pivoted.sql
+++ dbt_jardiner/models/operational/inverter/stg/lake_modbusreadings_inverter_pivoted.sql
@@ -1,11 +1,5 @@
 {{ config(materialized='view') }}
 
-{# modbus_unit = 3 assumes asomada at the moment, which is the unit of the inverter
-
-this code is for the registries 32bit metrics that are splitted into 2 modbus registries
- #}
-
-{# a fer demà : pivot tables #}
 with inverter_modbus_pivoted as (
     select
         five_minute,
@@ -15,11 +9,6 @@ with inverter_modbus_pivoted as (
     where five_minute is not NULL
     group by five_minute, device_type, device_id
 )
-{# do
-high_bytes > 2^15 then -1*(high_bytes*2^16 + low_bytes)
-
-add a comment that this assumes signed registries
- #}
 
 select
     *
