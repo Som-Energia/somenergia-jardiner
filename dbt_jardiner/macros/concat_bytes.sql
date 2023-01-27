8204b98d5384f57a50df3e7239e1fdb54af0108a
pol <pol.monso@somenergia.coop>
2023-01-27 20:01:23 +0100

    fix concat_bytes macro by complement 2

--- dbt_jardiner/macros/concat_bytes.sql
+++ dbt_jardiner/macros/concat_bytes.sql
@@ -1,10 +1,5 @@
 {% macro concat_bytes(column_name) %}
 
-case
-when {{column_name}}_highbyte > 2^15 then
-    -1*({{column_name}}_highbyte*2^16 + {{column_name}}_lowbyte)
-else
-    {{column_name}}_highbyte*2^16 + {{column_name}}_lowbyte
-end as {{column_name}}
+{{column_name}}_highbyte*2^16 + {{column_name}}_lowbyte - 2^32*({{column_name}}_highbyte > 2^15) as {{column_name}}
 
 {% endmacro %}
