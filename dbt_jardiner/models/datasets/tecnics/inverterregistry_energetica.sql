905a62d1fe324fa42520a6a6e623e913e3125721
Lugadur <lucia.garcia@somenergia.coop>
2023-01-30 14:41:29 +0100

    average_yearly_production_plant model for redash

--- dbt_jardiner/models/datasets/tecnics/inverterregistry_energetica.sql
+++
@@ -1,4 +0,0 @@
-{{ config(post_hook="grant select on {{ this }} to group energetica") }}
-
-select * from {{ ref('inverterregistry_clean') }}
-where plant_id in (select id from {{ ref('plants_energetica') }})
