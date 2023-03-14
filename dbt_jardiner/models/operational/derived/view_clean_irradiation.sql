1d280e7aa603f1c5a6ec9857a803ff84ca01175a
pol <pol.monso@somenergia.coop>
2023-03-14 14:15:48 +0100

    add legacy view_clean_irradiation for compatibility with some redash queries

---
+++ dbt_jardiner/models/operational/derived/view_clean_irradiation.sql
@@ -0,0 +1,25 @@
+{#
+    TODO decide:
+delete this view and do it in redash instead, with proper filters
+[chosen option] or hope that it's true that the planner limits the rows necessary by the subquery given a parent where clause
+Time it to decide
+#}
+
+{# this view is a legacy view which #}
+ SELECT sir.sensor,
+    sir."time",
+    COALESCE(sir.irradiation_wh_m2, sat.global_tilted_irradiation_wh_m2) AS irradiation_wh_m2_coalesce,
+    sir.irradiation_wh_m2 AS sonda_irradiation_wh_m2,
+    sat.global_tilted_irradiation_wh_m2 AS satellite_irradiation_wh_m2,
+        CASE
+            WHEN sir.quality < 1 THEN sat.global_tilted_irradiation_wh_m2
+            ELSE sir.irradiation_wh_m2
+        END AS irradiation_wh_m2,
+        CASE
+            WHEN sir.quality < 1 THEN 'satellite'
+            ELSE 'sonda'
+        END AS reading_source
+   FROM {{ source('plantmonitordb', 'irradiationregistry') }} sir
+     LEFT JOIN {{ source('plantmonitordb', 'sensor') }} sensor ON sensor.id = sir.sensor
+     LEFT JOIN {{ source('plantmonitordb', 'plant') }} plant ON plant.id = sensor.plant
+     LEFT JOIN {{ source('plantmonitordb', 'satellite_readings') }} sat ON sat.plant = sensor.plant AND time_bucket('1 hour', sat."time") = time_bucket('1 hour', sir."time")
