2cf86ec9e55f4afd481fcc397d998278fa53fc7a
Lugadur <lucia.garcia@somenergia.coop>
2023-10-25 13:44:30 +0200

    added new plant paramenters source

--- dbt_jardiner/models/jardiner/marts/dm_plants.sql
+++ dbt_jardiner/models/jardiner/marts/dm_plants.sql
@@ -1,13 +1,19 @@
 {{ config(materialize='view') }}
 
 select
-    plant_name as nom_planta,
+    plant as nom_planta,
+    municipality as municipi,
+    province as provincia,
     latitude as latitud,
     longitude as longitud,
     peak_power_kw as potencia_pic_kw,
     nominal_power_kw as potencia_nominal_kw,
     technology as tecnologia,
-    connection_date as data_connexio,
-    target_monthly_energy_gwh as energia_mensual_objectiu,
-    "owner" as empresa_manteniment
-from {{ ref('seed_plants__parameters') }}
+    connection_date as connexio,
+    owner as manteniment,
+    n_strings_plant as "strings/planta",
+    n_modules_string as "moduls/string",
+    n_strings_inverter as "strings/inversor",
+    esquema_unifilar,
+    layout
+from {{ ref('raw_gestio_actius_plant_parameters') }}
--- dbt_jardiner/models/jardiner/raw/gestio_actius/raw_gestio_actius_plant_parameters.sql
+++ dbt_jardiner/models/jardiner/raw/gestio_actius/raw_gestio_actius_plant_parameters.sql
@@ -3,20 +3,26 @@
 with current_data as (
     SELECT
         plant_id::numeric,
-        planta as planta,
+        case
+            when planta = 'Vallehermoso' then 'Alcolea'
+            when planta = 'Tahal' then 'Terborg'
+            else planta
+        end as plant,
         latitut::numeric as latitude,
         longitut::numeric as longitude,
         municipi as municipality,
         provincia as province,
         tecnologia as technology,
-        data_connexio::date as connection_data,
+        data_connexio::date as connection_date,
         potencia_nominal_kw::numeric as nominal_power_kw,
-        potencia_pic_kw::numeric,
+        potencia_pic_kw::numeric as peak_power_kw,
         owner,
         n_strings_plant::numeric,
         n_modules_string::numeric,
         n_strings_inverter::numeric,
         is_tilted::boolean,
+        esquema_unifilar,
+        layout,
         data_actualitzacio::date as gestio_actius_updated_at,
         dbt_updated_at::date as dbt_updated_at,
         dbt_valid_from::date as dbt_valid_from,
