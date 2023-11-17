016c5576e7dffdba4e8b1e29171ffac46e663e74
Pol Monsó Purtí <pol.monso@somenergia.coop>
2023-11-17 11:31:44 +0100

    chore: document airbyte omie historical price hour change from fr to incremental (et/somenergia-jardiner!27)
    * chore: document airbyte omie historical price hour change from fr to incremental

--- dbt_jardiner/models/jardiner/raw/omie/raw_plantlake_omie_historical_price__with_row_number_per_date.sql
+++ dbt_jardiner/models/jardiner/raw/omie/raw_plantlake_omie_historical_price__with_row_number_per_date.sql
@@ -1,31 +1,16 @@
-{{ config(
-    materialized = 'view'
-) }}
-{#
-todo airbyte IS currently doing A full - refresh which DROP - cascades,
-we materialize this model TO avoid checking if we can incremental - sync instead but it would NOT happen if we don 'T normalize the data, which we don' t actually need TO DO https:// discuss.airbyte.io / t / remove - related - VIEWS - IN - full - refresh - overwrite - MODE / 747 #}
-WITH recent AS (
+{{ config(materialized="view") }}
 
-    SELECT
-        "date",
-        create_time,
-        price,
-        ROW_NUMBER() OVER (
-            PARTITION BY "date"
-            ORDER BY
-                create_time DESC
-        ) AS rc
-    FROM
-        {{ source(
-            'plantlake',
-            'omie_historical_price_hour'
-        ) }}
-)
+with
+    recent as (
 
-SELECT
-    "date" AS start_hour,
-    price
-FROM
-    recent
-WHERE
-    rc = 1
+        select
+            "date",
+            create_time,
+            price,
+            row_number() over (partition by "date" order by create_time desc) as rc
+        from {{ source("plantlake", "omie_historical_price_hour") }}
+    )
+
+select "date" as start_hour, price
+from recent
+where rc = 1
