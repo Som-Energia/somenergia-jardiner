5c923ee7d66d9c9de82d28b9bad4276e3ec59897
Diego Quintana <diego.quintana@somenergia.coop>
2023-11-23 13:45:27 +0100

    ci: update dbt test should run in pre and prod depending on context (et/somenergia-jardiner!33)
    * chore: lint test not passing before fixing it. Related to #76
    
    * fix: missing dbt deps in task
    
    * change: avoid merging arrays in yaml
    
    * fix: extending arrays properly
    
    extending arrays in yaml is clunky, see https://github.com/yaml/yaml/issues/35
    
    * new: add dbt build instead of dbt test, parametrized by context
    
    during merge requests, use target pre
    on main, use target prod
    
    * new: make dbt run when there are changes in dbt models

--- dbt_jardiner/tests/test_repeated_device_uuid_errors.sql
+++ dbt_jardiner/tests/test_repeated_device_uuid_errors.sql
@@ -1,15 +1,19 @@
 -- tests pairs device_uuid, plant, device, device_type, device_parent as unique
-with tb as (
-    select distinct device_uuid, plant, device, device_type, device_parent
-    from {{ ref('raw_gestio_actius__signal_denormalized') }}
-    order by plant, device),
+with
+    tb as (
+        select distinct device_uuid, plant, device, device_type, device_parent
+        from {{ ref("raw_gestio_actius__signal_denormalized") }}
+        order by plant, device
+    ),
+
 tb2 as (
-    select tb.device_uuid, count(*) as duplicates
-    from tb
-    group by device_uuid
-    having count(*)> 1
+      select tb.device_uuid, count(*) as duplicates
+      from tb
+      group by tb.device_uuid
+      having count(*) > 1
     )
-select r.*, duplicates
-from {{ ref('raw_gestio_actius__signal_denormalized') }} r
-left join tb2 on tb2.device_uuid = r.device_uuid
-where duplicates is not null
+
+select r.*, tb2.duplicates
+from {{ ref("raw_gestio_actius__signal_denormalized") }} as r
+left join tb2 on r.device_uuid = tb2.device_uuid
+where tb2.duplicates is not null
