6f42763ab415fed7bebaea49f162b44c9798b7e4
Lucía <lucia.garcia@somenergia.coop>
2023-12-19 13:04:17 +0100

    fix/dades fixes de plantes (et/somenergia-jardiner!63)
    * fix raw dades fixes from plantmonitor (legacy)
    
    * fix/scheme reference
    
    * fix/dades fixes de plantes

--- dbt_jardiner/models/jardiner/intermediate/gestio_actius/int_gda_plants__plants_catalog.sql
+++ dbt_jardiner/models/jardiner/intermediate/gestio_actius/int_gda_plants__plants_catalog.sql
@@ -10,11 +10,14 @@ select
     peak_power_kw,
     nominal_power_kw,
     technology,
+    subtechnology,
     connection_date,
-    "owner",
+    owner,
+    maintenance_company,
+    manager,
     n_strings_plant,
-    n_modules_string,
-    n_strings_inverter,
+    n_modules_total,
+    n_inverters,
     esquema_unifilar,
     layout
 from {{ ref('raw_gestio_actius_plant_parameters') }}
--- dbt_jardiner/models/jardiner/marts/dm_dset__all_signals_received_per_plant.sql
+++ dbt_jardiner/models/jardiner/marts/dm_dset__all_signals_received_per_plant.sql
@@ -12,6 +12,6 @@ select
     ) as senyals_no_rebudes,
     count(*) filter (where uuid_senyal is not null) as senyals_esperades,
     count(*) as total_count
-from dbt_dev.dm_dset_last_reading__from_signal_last_ts
+from {{ref('dm_dset_last_reading__from_signal_last_ts')}}
 group by nom_planta, nom_planta_dset
 order by nom_planta asc, nom_planta_dset asc
