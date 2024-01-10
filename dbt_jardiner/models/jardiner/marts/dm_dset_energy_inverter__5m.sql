50f85e101f875df96c3e2880ad8a8f8d78249fc9
pol <pol.monso@somenergia.coop>
2024-01-10 15:38:22 +0100

    hotfix: signal_uuid distinguishes inverters :P

--- dbt_jardiner/models/jardiner/marts/dm_dset_energy_inverter__5m.sql
+++ dbt_jardiner/models/jardiner/marts/dm_dset_energy_inverter__5m.sql
@@ -7,10 +7,9 @@ with
             plant_uuid,
             plant_name,
             metric_name,
-            signal_uuid,
             sum(inverter_energy_kwh) as inverter_energy_kwh
         from {{ ref("int_dset_energy_inverter__5m") }}
-        group by ts, plant_uuid, plant_name, metric_name, signal_uuid
+        group by ts, plant_uuid, plant_name, metric_name
     )
 select *
 from inverters_energy
