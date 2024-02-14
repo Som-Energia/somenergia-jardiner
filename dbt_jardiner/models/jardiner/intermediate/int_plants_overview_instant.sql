5163b5c9ce9ac6f40b18729b2a812f2fd3a77b7a
Lucía <lucia.garcia@somenergia.coop>
2024-02-14 16:17:12 +0100

    Fix/instant data (et/somenergia-jardiner!106)
    * added superset handlebars code and fix columns names
    
    * rename int to english, catalan in dm
    
    * feature:limit sum of power to the currrent time
    
    * chanced route to get instant plant metrics, and deleted depreciated models

--- dbt_jardiner/models/jardiner/intermediate/int_plants_overview_instant.sql
+++ dbt_jardiner/models/jardiner/intermediate/int_plants_overview_instant.sql
@@ -1,46 +1,83 @@
 {{ config(materialized='view') }}
 
-
-with pot_instantanea_planta as (
+{# Assumim que una planta o bé té potencia_activa o bé té les tres fases potencia_activa_fase#}
+with plant_power_sum as (
     select
         plant_uuid,
-        min(ts) as pot_instantanea_planta_kw_last_registered_at,
-        round(sum(signal_value), 1) as pot_instantanea_planta_kw
+        min(ts) as instant_power_plant_kw_registered_at,
+        max(ts) - min(ts) as diff_ts,
+        round(sum(signal_value), 1) as instant_power_plant_kw
     from {{ ref('int_dset__last_registries') }}
+    where ts at time zone 'Europe/Madrid' >= current_date
+        and device_type = 'inverter'
+        and metric_name ilike 'potencia_activa%'
     group by
         plant_uuid,
-        device_type,
-        signal_name
-    having signal_name = 'potencia_activa'
+        device_type
+),
+
+instant_power_plant as (
+    select *
+    from plant_power_sum
+    where diff_ts <= interval '15 min'
+),
+
+last_irradiancia as (
+    {#
+        Cada planta té un o diversos tipus de senyals d'irradancia. Així, per cada planta, triem una senyal
+        d'irradancia. l'Order by permet fer una jerarquia quan a una planta hi ha més d'un tipus de irradància.
+    #}
+    select distinct
+        on (plant_uuid, ts)
+        plant_uuid,
+        ts,
+        signal_value
+    from {{ ref("int_dset__last_registries") }}
+    where
+        metric_name = 'irradiancia'
+        and ts at time zone 'Europe/Madrid' >= current_date
+    order by
+        plant_uuid,
+        ts,
+        case
+            when signal_name = 'irradiancia'
+            then 1
+            when signal_name = 'irradiancia_sonda_bruta'
+            then 2
+            when signal_name = 'irradiancia_sonda_neta'
+            then 3
+            when signal_name = 'irradiancia_sonda'
+            then 4
+        end
 ),
 
 plant_production_daily_previous_day as (
     select
         plant_uuid,
-        nom_planta,
-        dia,
-        energia_exportada_comptador_kwh,
-        energia_esperada_solargis_kwh
+        nom_planta as plant_name,
+        dia as "day",
+        energia_exportada_comptador_kwh as meter_exported_energy_kwh,
+        energia_esperada_solargis_kwh as solargis_meter_expected_energy_kwh
     from {{ ref('dm_plant_production_daily') }}
     where dia = current_date - interval '1 day'
 )
 
 select
     p.plant_uuid,
-    p.plant_name as nom_planta,
-    p.municipality as municipi,
-    p.province as provincia,
-    p.technology as tecnologia,
-    p.peak_power_kw as potencia_pic_kw,
-    p.nominal_power_kw as potencia_nominal_kw,
-    i.pot_instantanea_planta_kw_last_registered_at,
-    i.pot_instantanea_planta_kw,
-    ir.ts as irradiacio_w_m2_last_registered_at,
-    ir.signal_value as irradiacio_w_m2,
-    ppd.dia,
-    ppd.energia_exportada_comptador_kwh,
-    ppd.energia_esperada_solargis_kwh
+    p.plant_name,
+    p.municipality,
+    p.province,
+    p.technology,
+    p.peak_power_kw,
+    p.nominal_power_kw,
+    i.instant_power_plant_kw_registered_at,
+    i.instant_power_plant_kw,
+    ir.ts as irradiance_w_m2_last_registered_at,
+    ir.signal_value as irradiance_w_m2,
+    ppd.day,
+    ppd.meter_exported_energy_kwh,
+    ppd.solargis_meter_expected_energy_kwh
 from {{ ref('raw_gestio_actius_plant_parameters') }} as p
-left join pot_instantanea_planta as i on p.plant_uuid = i.plant_uuid
+left join instant_power_plant as i on p.plant_uuid = i.plant_uuid
 left join plant_production_daily_previous_day as ppd on p.plant_uuid = ppd.plant_uuid
-left join {{ ref('int_dset_last_registries_irradiation') }} as ir on p.plant_uuid = ir.plant_uuid
+left join last_irradiancia as ir on p.plant_uuid = ir.plant_uuid
--- dbt_jardiner/models/jardiner/marts/dm_dashboard_overview_instant.sql
+++ dbt_jardiner/models/jardiner/marts/dm_dashboard_overview_instant.sql
@@ -1,15 +1,16 @@
 {{ config(materialized="view") }}
 
 select
-    nom_planta,
-    provincia,
-    tecnologia,
-    potencia_pic_kw,
-    pot_instantanea_planta_kw_last_registered_at,
-    irradiacio_w_m2_last_registered_at,
-    irradiacio_w_m2,
-    dia,
-    round(pot_instantanea_planta_kw, 2) as pot_instantanea_planta_kw,
-    round(pot_instantanea_planta_kw / potencia_pic_kw, 2) as instant_vs_pic,
-    round((energia_exportada_comptador_kwh - energia_esperada_solargis_kwh) / 1000, 2) as energia_perduda_mwh
-from {{ ref("int_plants_overview_instant") }}
+    overview.plant_name as nom_planta,
+    overview.province as provincia,
+    overview.technology as tecnologia,
+    overview.peak_power_kw as potencia_pic_kw,
+    overview.nominal_power_kw as potencia_nominal_kw,
+    overview.irradiance_w_m2_last_registered_at as irradiancia_ts,
+    round(overview.irradiance_w_m2, 2) as irradiancia_w_m2,
+    overview.instant_power_plant_kw_registered_at as potencia_inst_ts,
+    round(overview.instant_power_plant_kw, 2) as potencia_inst_kw,
+    round(overview.instant_power_plant_kw / overview.peak_power_kw, 2) as potencia_inst_vs_pic,
+    overview.day as dia,
+    round((overview.meter_exported_energy_kwh - overview.solargis_meter_expected_energy_kwh) / 1000, 2) as energia_perduda_mwh
+from {{ ref("int_plants_overview_instant") }} as overview
