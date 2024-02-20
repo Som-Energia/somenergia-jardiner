5269a8d531b568eeefad1bf83f12e18529266b20
Pol Monsó Purtí <pol.monso@somenergia.coop>
2024-02-20 15:56:44 +0100

    Resolve "Rang de temps del gràfic Evolució mes a mes" (et/somenergia-jardiner!110)
    * use only latest snapshot for target, greatly simplify dm_dashboard_overview_monthly
    
    * refactor dm_dashboard_overview_monthly by agregating by technology at the last cte
    
    * sqlfluff dm_plant_production_monthly
    
    * add docs on dm_dashboard_overview_*
    
    * update produccio avui html+css, propagate plant_uuid from objectius
    
    * add a TODO for monday

--- dbt_jardiner/models/jardiner/intermediate/int_production_target__long.sql
+++ dbt_jardiner/models/jardiner/intermediate/int_production_target__long.sql
@@ -3,12 +3,20 @@
 
 with objectius_unpivoted as (
   {# Pivot values from columns to rows. Similar to pandas DataFrame melt() function.#}
-  {{ dbt_utils.unpivot(relation=ref('raw_gestio_actius_production_target'), cast_to='numeric', exclude=['plant_name','gestio_actius_updated_at','dbt_updated_at','dbt_valid_from','dbt_valid_to'], field_name='month', value_name='energy_production_target_mwh') }}
+  {{ dbt_utils.unpivot(
+      relation=ref('raw_gestio_actius_production_target'),
+      cast_to='numeric',
+      exclude=['plant_name','plant_uuid','gestio_actius_updated_at','dbt_updated_at','dbt_valid_from','dbt_valid_to'],
+      field_name='month',
+      value_name='energy_production_target_mwh'
+      )
+  }}
 )
 select
-  plant_name,
-  month,
-  energy_production_target_mwh,
-  dbt_valid_from,
-  dbt_valid_to
+  objectius_unpivoted.plant_name,
+  objectius_unpivoted.plant_uuid,
+  objectius_unpivoted.month,
+  objectius_unpivoted.energy_production_target_mwh,
+  objectius_unpivoted.dbt_valid_from,
+  objectius_unpivoted.dbt_valid_to
 from objectius_unpivoted
--- dbt_jardiner/models/jardiner/intermediate/int_production_target__monthly.sql
+++ dbt_jardiner/models/jardiner/intermediate/int_production_target__monthly.sql
@@ -4,9 +4,9 @@
 select
   s.month,
   pt.plant_name,
+  pt.plant_uuid,
   pt.energy_production_target_mwh
 from {{ ref('spine_monthly__full_year') }} as s
   left join {{ ref('int_production_target__long') }} as pt
     on
-      s.month >= pt.dbt_valid_from and s.month < pt.dbt_valid_to
-      and to_char(s.month, 'FMmonth') ilike pt.month
+      to_char(s.month, 'FMmonth') ilike pt.month
