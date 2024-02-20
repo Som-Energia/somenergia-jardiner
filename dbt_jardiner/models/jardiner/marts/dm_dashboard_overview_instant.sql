06f173241c883f7645fc8bede0c30cca0a2a42ee
Pol Monsó Purtí <pol.monso@somenergia.coop>
2024-02-20 17:35:17 +0100

    Resolve "Plants overview now - columna Potencia instantia vs potencia pic" (et/somenergia-jardiner!113)
    * Closes #172, update handlebars produccio avui
    
    * instant vs pic is now instant vs nominal

--- dbt_jardiner/models/jardiner/marts/dm_dashboard_overview_instant.sql
+++ dbt_jardiner/models/jardiner/marts/dm_dashboard_overview_instant.sql
@@ -1,16 +1,16 @@
 {{ config(materialized="view") }}
 
 select
-    overview.plant_name as nom_planta,
-    overview.province as provincia,
-    overview.technology as tecnologia,
-    overview.peak_power_kw as potencia_pic_kw,
-    overview.nominal_power_kw as potencia_nominal_kw,
-    overview.irradiance_w_m2_last_registered_at as irradiancia_ts,
-    round(overview.irradiance_w_m2, 2) as irradiancia_w_m2,
-    overview.instant_power_plant_kw_registered_at as potencia_inst_ts,
-    round(overview.instant_power_plant_kw, 2) as potencia_inst_kw,
-    round(overview.instant_power_plant_kw / overview.peak_power_kw, 2) as potencia_inst_vs_pic,
-    overview.day as dia,
-    round((overview.meter_exported_energy_kwh - overview.solargis_meter_expected_energy_kwh) / 1000, 2) as energia_perduda_mwh
+  overview.plant_name as nom_planta,
+  overview.province as provincia,
+  overview.technology as tecnologia,
+  overview.peak_power_kw as potencia_pic_kw,
+  overview.nominal_power_kw as potencia_nominal_kw,
+  overview.irradiance_w_m2_last_registered_at as irradiancia_ts,
+  round(overview.irradiance_w_m2, 2) as irradiancia_w_m2,
+  overview.instant_power_plant_kw_registered_at as potencia_inst_ts,
+  round(overview.instant_power_plant_kw, 2) as potencia_inst_kw,
+  round(overview.instant_power_plant_kw / overview.nominal_power_kw, 2) as potencia_inst_vs_nominal,
+  overview.day as dia,
+  round((overview.meter_exported_energy_kwh - overview.solargis_meter_expected_energy_kwh) / 1000, 2) as energia_perduda_mwh
 from {{ ref("int_plants_overview_instant") }} as overview
