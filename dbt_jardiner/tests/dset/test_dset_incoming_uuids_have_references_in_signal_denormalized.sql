c94eab10f17985a21592dcde4db15bebc92173b5
Pol Monsó Purtí <pol.monso@somenergia.coop>
2024-02-22 12:57:26 +0100

    chore: speedup slow dbt tests (et/somenergia-jardiner!68)
    * disable mandatory dbt build test pass on pre MR
    
    * remove deprecated models with long tests
    
    * limit simel tests to last two days
    
    * limit slow tests to one month

--- dbt_jardiner/tests/dset/test_dset_incoming_uuids_have_references_in_signal_denormalized.sql
+++ dbt_jardiner/tests/dset/test_dset_incoming_uuids_have_references_in_signal_denormalized.sql
@@ -1,38 +1,41 @@
 {{ config(severity="warn") }}
 
 with
-    child as (
-        select *, signal_uuid as signal_uuid_dset
-        from {{ ref("int_dset_responses__materialized") }}
-        where signal_uuid is not null
-    ),
+child as (
+  select *, signal_uuid as signal_uuid_dset
+  from {{ ref("int_dset_responses__materialized") }}
+  where
+    signal_uuid is not null
+    {# limit test backcheck to speed up testing #}
+    and queried_at > now() - interval '1 month'
+),
 
-    parent as (
-      select signal_uuid as signal_uuid_som
-      from {{ ref("raw_gestio_actius__signal_denormalized") }}
-    ),
+parent as (
+  select signal_uuid as signal_uuid_som
+  from {{ ref("raw_gestio_actius__signal_denormalized") }}
+),
 
-    joined as (
-        select child.*, parent.*
-        from child
-        left join parent on child.signal_uuid_dset = parent.signal_uuid_som
-    ),
+joined as (
+  select child.*, parent.*
+  from child
+    left join parent on child.signal_uuid_dset = parent.signal_uuid_som
+),
 
-    unified as (
-        select distinct on (signal_uuid_dset, signal_uuid_som)
-            *,
-            case
-                when signal_uuid_som is null then 'missing_from_som'
-                when signal_uuid_dset is null then 'extra_from_dset'
-                else 'ok'
-            end as status
-        from joined
-    ),
+unified as (
+  select distinct on (signal_uuid_dset, signal_uuid_som)
+    *,
+    case
+      when signal_uuid_som is null then 'missing_from_som'
+      when signal_uuid_dset is null then 'extra_from_dset'
+      else 'ok'
+    end as status
+  from joined
+),
 
-    filtered as (
-        select *
-        from unified
-        where status != 'ok'
-    )
+filtered as (
+  select *
+  from unified
+  where status != 'ok'
+)
 
 select * from filtered
