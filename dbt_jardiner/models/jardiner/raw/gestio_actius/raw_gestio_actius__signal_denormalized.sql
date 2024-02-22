90b250fbf89b046373b8482f1f7ad314e326c22d
Pol Monsó Purtí <pol.monso@somenergia.coop>
2024-02-22 12:09:18 +0100

    trim raw_GA__signal_denormalized text fields (et/somenergia-jardiner!121)
    * fix typo in docs
    
    * disable referential integrity with plantmonitor correctly
    
    * disable plantmonitor plant uuid integrity with jardiner
    
    * trim raw_GA__signal_denormalized text fields

--- dbt_jardiner/models/jardiner/raw/gestio_actius/raw_gestio_actius__signal_denormalized.sql
+++ dbt_jardiner/models/jardiner/raw/gestio_actius/raw_gestio_actius__signal_denormalized.sql
@@ -1,26 +1,26 @@
 {{ config(materialized="view") }}
 
 with
-    fresh as (
-      select *
-      from {{ ref("snapshot_signal_denormalized") }}
-      where dbt_valid_to is null
-    ),
+fresh as (
+  select *
+  from {{ ref("snapshot_signal_denormalized") }}
+  where dbt_valid_to is null
+),
 
-    base as (
-        select
-          plant_uuid::uuid,
-          plant::text as plant_name,
-          signal::text as signal_name,
-          metric::text as metric_name,
-          device::text as device_name,
-          device_type::text,
-          device_uuid::uuid,
-          device_parent::text,
-          signal_uuid::uuid,
-          inserted_at::timestamptz,
-          updated_at::timestamptz
-        from fresh
-    )
+base as (
+  select
+    plant_uuid::uuid,
+    trim(plant::text) as plant_name,
+    trim(signal::text) as signal_name,
+    metric::text as metric_name,
+    trim(device::text) as device_name,
+    device_type::text,
+    device_uuid::uuid,
+    trim(device_parent::text) as device_parent,
+    signal_uuid::uuid,
+    inserted_at::timestamptz,
+    updated_at::timestamptz
+  from fresh
+)
 
 select * from base
